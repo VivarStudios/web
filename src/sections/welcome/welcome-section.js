c292fd30d5004f0f1ed52a6a86d3f75f05fa1bc7
miguelangelfuentes <miguelangelfuentes@users.noreply.github.com>
2026-08-10 20:42:50 +0200

    Feature/improve content (#3)
    * starting new version
    
    * adding sopabase

--- src/sections/welcome/welcome-section.js
+++ src/sections/welcome/welcome-section.js
@@ -1,14 +1,10 @@
 import welcomeHtml from './welcome-section.html?raw';
-import Experience from '/Experience/Experience.js'
-
 
 class WelcomeSection extends HTMLElement {
     connectedCallback() {
         if (!this.innerHTML) {
             this.innerHTML = welcomeHtml;
         }
-
-        const experience = new Experience(this.querySelector('canvas.webgl'))
     }
 }
 
