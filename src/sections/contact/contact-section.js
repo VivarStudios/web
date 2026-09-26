7ebf22efb6b0227f2eced4393f5bef0561d7614b
Miguel Angel Fuentes Cardenas <miguelangel.fuentes1985@gmail.com>
2026-09-26 21:34:10 +0200

    contact alert

--- src/sections/contact/contact-section.js
+++ src/sections/contact/contact-section.js
@@ -1,38 +1,81 @@
 import contactHTML from './contact-section.html?raw';
 
 class ContactSection extends HTMLElement {
+    timeOut3Seconds;
+
     connectedCallback() {
         if (!this.innerHTML) {
             this.innerHTML = contactHTML;
         }
+        if (!this.modalLoading) {
+            this.modalLoading = this.querySelector('#contact-modalLoading');
+        }
+        if (!this.modalSuccess) {
+            this.modalSuccess = this.querySelector('#contact-modalSuccess');
+        }
+        if (!this.modalError) {
+            this.modalError = this.querySelector('#contact-modalError');
+        }
 
-        const form = document.querySelector('#contactForm');
-
+        const form = this.querySelector('#contactForm');
         form.addEventListener('submit', (e) => {
             e.preventDefault();
             this.sendEmail({
                 name: form.name.value,
                 email: form.email.value,
-                message: form.message.value
+                message: form.message.value,
             });
         });
     }
 
-    sendEmail(data) {
-        fetch('/send-mail.php', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(data)
-        })
-        .then((response) => response?.json())
-        .then((data) => {
-            console.log('Success:', data);
-            alert('¡Enviado!');
-        })
-        .catch((error) => {
+    async sendEmail(data) {
+        clearTimeout(this.timeOut3Seconds);
+
+        this.modalLoading.showModal();
+
+        const controller = new AbortController();
+        const requestTimeout = setTimeout(() => {
+            controller.abort();
+            this.closeModal(this.modalLoading);
+            this.showErrorModal();
+        }, 10000);
+
+        try {
+            const response = await fetch('/send-mail.php', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(data),
+                signal: controller.signal,
+            });
+
+            if (response.ok) {
+                this.showModal(this.modalSuccess);
+            } else {
+                this.showModal(this.modalError);
+            }
+        } catch (error) {
             console.error('Error:', error);
-            alert('Mi Error: ' + error);
-        });
+            this.showModal(this.modalError);
+        } finally {
+            clearTimeout(requestTimeout);
+            this.closeModal(this.modalLoading);
+        }
+    }
+
+    closeModal(modal) {
+        modal?.close();
+    }
+
+    showModal(modal) {
+        if (!modal) {
+            return;
+        }
+
+        clearTimeout(this.timeOut3Seconds);
+        modal.showModal();
+        this.timeOut3Seconds = setTimeout(() => {
+            modal.close();
+        }, 3000);
     }
 }
 
